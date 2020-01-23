e1b5121db6fa0bed00f8353a308957598c7b3f1c
Alex <ch.alexandre.reed@gmail.com>
2020-01-23 17:35:03 +0100

    video loader in webpack config.js

--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,22 @@ let config = {
                 ]
             },
             {
+                // Now we apply rule for images
+                test: /\.(mp4|avi|mov|mkv)$/,
+                use: [
+                    {
+                        // Using file-loader for these files
+                        loader: "file-loader",
+
+                        // In options we can set different things like format
+                        // and directory to save
+                        options: {
+                            outputPath: 'videos'
+                        }
+                    }
+                ]
+            },
+            {
                 // Apply rule for fonts files
                 test: /\.(woff|woff2|ttf|otf|eot)$/,
                 use: [
